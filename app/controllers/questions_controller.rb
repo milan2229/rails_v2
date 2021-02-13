d2041cc319e31c03472c549c0f556b36d5bb91d0
milan.1899 <milan_1899.22@icloud.com>
2021-02-13 15:49:47 +0000

    finish commit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,12 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :update, :edit, :update]
+
   def index
     @questions = Question.all
   end
 
   def show
-    @question = Question.find(params[:id])
+    # @question = Question.find(params[:id])
     @answer = Answer.new
   end
 
@@ -23,11 +25,11 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    @question = Question.find(params[:id])
+    # @question = Question.find(params[:id])
   end
 
   def update
-    @question = Question.find(params[:id])
+    # @question = Question.find(params[:id])
     if @question.update(question_params)
       redirect_to root_path, notice: 'Success'
     else
@@ -37,13 +39,18 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question = Question.find(params[:id])
+    # @question = Question.find(params[:id])
     @question.destroy
     redirect_to root_path, notice: 'succes!!!'
   end
 
 
   private
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
   def question_params
     params.require(:question).permit(:name, :title, :content)
   end
